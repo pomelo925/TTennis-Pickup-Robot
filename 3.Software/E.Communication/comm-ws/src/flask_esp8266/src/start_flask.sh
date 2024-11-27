ea248a2c899e4ed9a244101e829913171be49f3c
pomelo925 <yoseph.huang@gmail.com>
2024-11-27 20:52:15 +0800

    coordinator dockerfile

--- 3.Software/E.Communication/comm-ws/src/flask_esp8266/src/start_flask.sh
+++ 3.Software/E.Communication/comm-ws/src/flask_esp8266/src/start_flask.sh
@@ -6,4 +6,13 @@ source ~/.bashrc
 pip install Flask
 sudo apt-get --reinstall install python3-requests
 
+# run python file
 python app.py
+
+# run ros file at another terminal
+
+# cd /root/comm-ws &&
+# source devel/setup.bash
+# chmod +x /root/comm-ws/src/flask_esp8266/scripts/button_press_publisher.py
+
+# rosrun flask_esp8266 button_press_publisher.py
